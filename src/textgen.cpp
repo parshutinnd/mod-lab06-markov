84119b3cee30488a1d5fd4a7a6e620725ed2f218
parshutinnd <52651116+parshutinnd@users.noreply.github.com>
2024-06-05 17:31:45 +0300

    Update textgen.cpp cpplint

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -7,26 +7,27 @@
 #include <random>
 #include  <iterator>
 
-std::vector<std::string>::iterator TextGen::select_randomly(std::vector<std::string>::iterator start, std::vector<std::string>::iterator end, std::mt19937& g){
+std::vector<std::string>::iterator TextGen::select_randomly(std::vector<std::string>::iterator start, 
+                                                            std::vector<std::string>::iterator end, std::mt19937& g) {
     std::uniform_int_distribution<> dis(0, std::distance(start, end) - 1);
     std::advance(start, dis(g));
     return start;
 }
 
-std::vector<std::string>::iterator TextGen::select_randomly(std::vector<std::string>::iterator start, std::vector<std::string>::iterator end){
+std::vector<std::string>::iterator TextGen::select_randomly(std::vector<std::string>::iterator start, 
+                                                            std::vector<std::string>::iterator end) {
     static std::random_device rd;
     static std::mt19937 gen(rd());
     return select_randomly(start, end, gen);
 }
 
-TextGen::TextGen(std::string filePath){
+TextGen::TextGen(std::string filePath) {
     std::string line;
 
     std::ifstream in(filePath); // окрываем файл для чтения
-    if (in.is_open()){
+    if (in.is_open()) {
         while (std::getline(in, line));
-    }
-    else {
+    } else {
         std::cout << "cant open" << "\n";
     }
     in.close();     // закрываем файл
@@ -42,8 +43,7 @@ TextGen::TextGen(std::string filePath){
         if (line[i] != separator) {
             // Append the char to the temp string.
             s += line[i];
-        }
-        else {
+        } else {
             words.push_back(s);
             s.clear();
         }
@@ -53,13 +53,12 @@ TextGen::TextGen(std::string filePath){
     words.push_back("end");
     auto it = words.insert(words.begin(), "start");
 
-    for (int i = 0; i < words.size() - 1; i++){
+    for (int i = 0; i < words.size() - 1; i++) {
         std::string elem = words[i];
-        if (states.count(elem) == 0){
+        if (states.count(elem) == 0) {
             states[elem] = std::vector<std::string>();
             states[elem].push_back(words[i+1]);
-        }
-        else{
+        } else {
             states[elem].push_back(words[i+1]);
         }
     }
@@ -67,16 +66,15 @@ TextGen::TextGen(std::string filePath){
     states["end"] = std::vector<std::string>();
 }
 
-std::string TextGen::Generate(int n){
+std::string TextGen::Generate(int n) {
     std::string result;
     std::string curr = "start";
     
-    for(int i = 0; i < n;){
+    for(int i = 0; i < n;) {
         curr = *select_randomly(states[curr].begin(), states[curr].end());
         if (curr == "end") {
             curr = "start";
-        }
-        else {
+        } else {
             result += " ";
             result += curr;
             i++;
