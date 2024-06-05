4b9bf80c2fb09d34d8a68e6740820b2061e8d33b
Nikita Parshutin <niki.saransk2016@gmail.com>
2024-06-05 17:11:11 +0300

    cpplint again

--- include/textgen.h
+++ include/textgen.h
@@ -5,13 +5,15 @@
 #include <vector>
 #include <random>
 
+// Copyright 2024
+
 class TextGen{
  private:
  public:
-     TextGen(std::string);
+     TextGen::TextGen(std::string);
      std::map<std::string, std::vector<std::string>> states;
      std::string Generate(int n);
-     std::vector<std::string>::iterator 
+     std::vector<std::string>::iterator
      select_randomly(std::vector<std::string>::iterator start, std::vector<std::string>::iterator end);
      std::vector<std::string>::iterator
      select_randomly(std::vector<std::string>::iterator start, std::vector<std::string>::iterator end, std::mt19937& g);
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include "textgen.h"
+#include "/textgen.h"
 
-int main(){
+// Copyright 2024
+
+int main() {
     TextGen tg = TextGen("../../input.txt");
     std::cout << tg.Generate(50);
     return 0;
