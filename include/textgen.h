5fb988ba5843d2ec5db78c14da35aec1600bb3d5
parshutinnd <52651116+parshutinnd@users.noreply.github.com>
2024-06-05 17:35:43 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -10,12 +10,14 @@
 class TextGen{
  private:
  public:
-     TextGen(std::string);
+     explicit TextGen(std::string);
      std::map<std::string, std::vector<std::string>> states;
      std::string Generate(int n);
      std::vector<std::string>::iterator
-     select_randomly(std::vector<std::string>::iterator start, std::vector<std::string>::iterator end);
+     select_randomly(std::vector<std::string>::iterator start,
+                     std::vector<std::string>::iterator end);
      std::vector<std::string>::iterator
-     select_randomly(std::vector<std::string>::iterator start, std::vector<std::string>::iterator end, std::mt19937& g);
+     select_randomly(std::vector<std::string>::iterator start,
+     std::vector<std::string>::iterator end, std::mt19937& g);
 };
 
